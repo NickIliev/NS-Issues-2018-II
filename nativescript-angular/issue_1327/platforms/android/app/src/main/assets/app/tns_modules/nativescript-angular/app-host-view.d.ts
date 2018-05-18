912db9610d11ee232dc327a56cac6b8de4199b7e
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-05-18 13:42:26 +0300

    add

--- nativescript-angular/issue_1327/platforms/android/app/src/main/assets/app/tns_modules/nativescript-angular/app-host-view.d.ts
+++
@@ -1,5 +0,0 @@
-import { ContentView } from "tns-core-modules/ui/content-view";
-import { ViewBase } from "tns-core-modules/ui/core/view/view";
-export declare class AppHostView extends ContentView {
-    ngAppRoot: ViewBase;
-}
