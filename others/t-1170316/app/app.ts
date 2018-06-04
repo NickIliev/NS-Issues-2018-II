26e016710078e0c283e9e8472c4076096b9aec74
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-06-04 13:43:58 +0300

    add

--- others/t-1170316/app/app.ts
+++ others/t-1170316/app/app.ts
@@ -1,10 +1,29 @@
 ﻿import "./bundle-config";
 import * as application from 'application';
 
+require('nativescript-websockets');
+
+let mySocket: any;
+
 application.on(application.launchEvent, (args) => {
     if (args.android) {
         // For Android applications, args.android is an android.content.Intent class.
         console.log("Launched Android application with the following intent: " + args.android + ".");
+
+        mySocket = new WebSocket("ws://echo.websocket.org", [ /* "protocol","another protocol" */]);
+
+        mySocket.addEventListener('open', function (evt) { 
+            console.log("We are Open"); 
+        });
+
+        mySocket.addEventListener('close', function (evt) { 
+            console.log("The Socket was Closed:"); 
+        });
+
+        mySocket.addEventListener('error', function (evt) { 
+            console.log("The socket had an error") 
+        });
+
     } else if (args.ios !== undefined) {
         // For iOS applications, args.ios is NSDictionary (launchOptions).
         console.log("Launched iOS application with options: " + args.ios);
@@ -31,20 +50,15 @@ application.on(application.resumeEvent, (args) => {
     }
 });
 
-application.on(application.displayedEvent, (args) => {
-    // args is of type ApplicaitonEventData
-    console.log("displayedEvent");
-});
-
-application.on(application.orientationChangedEvent, (args) => {
-    // args is of type OrientationChangedEventData
-    console.log("ORIENTATION: ", args.newValue); // "portrait", "landscape", "unknown"
-});
-
 
 application.on(application.exitEvent, (args) => {
     if (args.android) {
         // For Android applications, args.android is an android activity class.
+
+
+        mySocket.close();
+ 
+
         console.log("EXIT: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is UIApplication.
@@ -65,30 +79,6 @@ application.on(application.lowMemoryEvent, (args) => {
 application.on(application.uncaughtErrorEvent, (args) => {
     console.log("Uncaught Error: " + args.error);
 });
-
-
-import { ios } from "application";
-
-class MyDelegate extends UIResponder implements UIApplicationDelegate {
-    public static ObjCProtocols = [UIApplicationDelegate];
-
-    applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary<string, any>): boolean {
-        console.log("applicationWillFinishLaunchingWithOptions: " + launchOptions)
-
-        return true;
-    }
-
-    applicationWillTerminate(application: UIApplication, ) {
-        console.log("WILL TERMINATE !!!");
-        console.log("applicationWillTerminate: ", application);
-    }
-
-    applicationDidBecomeActive(application: UIApplication): void {
-        console.log("applicationDidBecomeActive: " + application)
-    }
-}
-ios.delegate = MyDelegate;
-
 
 application.run({ moduleName: 'app-root' });
 
