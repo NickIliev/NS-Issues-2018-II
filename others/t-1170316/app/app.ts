dfd688c835c364dc0a7eab0288631695b6275257
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-06-05 09:58:45 +0300

    add

--- others/t-1170316/app/app.ts
+++ others/t-1170316/app/app.ts
@@ -1,88 +1,52 @@
 ﻿import "./bundle-config";
 import * as application from 'application';
+import * as LocalNotifications from "nativescript-local-notifications";
 
-require('nativescript-websockets');
+const WS = require("nativescript-websockets");
 
 let mySocket: any;
 
 application.on(application.launchEvent, (args) => {
-    if (args.android) {
-        // For Android applications, args.android is an android.content.Intent class.
-        console.log("Launched Android application with the following intent: " + args.android + ".");
+    LocalNotifications.hasPermission()
 
-        mySocket = new WebSocket("ws://echo.websocket.org", [ /* "protocol","another protocol" */]);
+    mySocket = new WS("ws://echo.websocket.org",{protocols: [/* 'chat', 'video' */], timeout: 6000, allowCellular: true, headers: { 'Authorization': 'Basic ...' }});
+    mySocket.on('open', (socket) => { console.log("Hey I'm open"); socket.send("Hello"); });
+    mySocket.on('message', (socket, message) => { 
+        console.log("Got a message", message); 
 
-        mySocket.addEventListener('open', function (evt) { 
-            console.log("We are Open"); 
-        });
+        LocalNotifications.schedule([{
+            id: 3,
+            title: 'Hi',
+            body: 'You should see a \'3\' somewhere',
+            at: new Date(new Date().getTime() + 10*1000),
+            badge: 3
+          }]).then(
+              () => {
+                alert({
+                  title: "Notification scheduled",
+                  message: 'ID: 3',
+                  okButtonText: "OK, thanks"
+                });
+              },(error) => {
+                console.log("doScheduleAndSetBadgeNumber error: " + error);
+              }
+          );
 
-        mySocket.addEventListener('close', function (evt) { 
-            console.log("The Socket was Closed:"); 
-        });
-
-        mySocket.addEventListener('error', function (evt) { 
-            console.log("The socket had an error") 
-        });
-
-    } else if (args.ios !== undefined) {
-        // For iOS applications, args.ios is NSDictionary (launchOptions).
-        console.log("Launched iOS application with options: " + args.ios);
-    }
-});
-
-application.on(application.suspendEvent, (args) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log("SUSPEND: " + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log("SUSPEND EVENT: " + args.ios);
-    }
+    });
+    mySocket.on('close', (socket, code, reason) => { console.log("Socket was closed because: ", reason, " code: ", code); });
+    mySocket.on('error', (socket, error) => { console.log("Socket had an error", error);});
 });
 
 application.on(application.resumeEvent, (args) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log("RESUME: " + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log("RESUME EVENT: " + args.ios);
-    }
+    mySocket.open();
+
+    console.log("RESUME");
 });
 
+application.on(application.suspendEvent, (args) => {
+    mySocket.close();
 
-application.on(application.exitEvent, (args) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-
-
-        mySocket.close();
- 
-
-        console.log("EXIT: " + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log("EXIT EVENT: " + args.ios);
-    }
-});
-
-application.on(application.lowMemoryEvent, (args) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log("LOW MEMORY: " + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log("LOW MEMORY: " + args.ios);
-    }
-});
-
-application.on(application.uncaughtErrorEvent, (args) => {
-    console.log("Uncaught Error: " + args.error);
+    console.log("SUSPEND");
 });
 
 application.run({ moduleName: 'app-root' });
-
-/*
-Do not place any code after the application has been started as it will not
-be executed on iOS.
-*/
