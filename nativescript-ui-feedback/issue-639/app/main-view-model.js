5d21fe6c6c603ae4e9283dcbfd911b5d834994f5
NickIliev <nikolay.iliev@telerik.com>
2018-06-06 14:17:07 +0300

    add

---
+++ nativescript-ui-feedback/issue-639/app/main-view-model.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var observable_1 = require("data/observable");
+var HelloWorldModel = /** @class */ (function (_super) {
+    __extends(HelloWorldModel, _super);
+    function HelloWorldModel() {
+        var _this = _super.call(this) || this;
+        _this.firstColor = "#dddddd";
+        _this.secondColor = "#9DCA56";
+        return _this;
+    }
+    return HelloWorldModel;
+}(observable_1.Observable));
+exports.HelloWorldModel = HelloWorldModel;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi12aWV3LW1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibWFpbi12aWV3LW1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsOENBQTZDO0FBRTdDO0lBQXFDLG1DQUFVO0lBSzNDO1FBQUEsWUFDSSxpQkFBTyxTQUlWO1FBRkcsS0FBSSxDQUFDLFVBQVUsR0FBRyxTQUFTLENBQUM7UUFDNUIsS0FBSSxDQUFDLFdBQVcsR0FBRyxTQUFTLENBQUM7O0lBQ2pDLENBQUM7SUFDTCxzQkFBQztBQUFELENBQUMsQUFYRCxDQUFxQyx1QkFBVSxHQVc5QztBQVhZLDBDQUFlIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgT2JzZXJ2YWJsZSB9IGZyb20gJ2RhdGEvb2JzZXJ2YWJsZSc7XG5cbmV4cG9ydCBjbGFzcyBIZWxsb1dvcmxkTW9kZWwgZXh0ZW5kcyBPYnNlcnZhYmxlIHtcblxuICAgIHB1YmxpYyBmaXJzdENvbG9yOiBzdHJpbmc7XG4gICAgcHVibGljIHNlY29uZENvbG9yOiBzdHJpbmc7XG5cbiAgICBjb25zdHJ1Y3RvcigpIHtcbiAgICAgICAgc3VwZXIoKTtcblxuICAgICAgICB0aGlzLmZpcnN0Q29sb3IgPSBcIiNkZGRkZGRcIjtcbiAgICAgICAgdGhpcy5zZWNvbmRDb2xvciA9IFwiIzlEQ0E1NlwiO1xuICAgIH1cbn1cbiJdfQ==
