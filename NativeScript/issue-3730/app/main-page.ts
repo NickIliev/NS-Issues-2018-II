0aedafb77136f3fbe7f2fe25ba0b16dcb09605d6
NickIliev <nikolay.iliev@telerik.com>
2018-07-05 14:41:34 +0300

    add

---
+++ NativeScript/issue-3730/app/main-page.ts
@@ -0,0 +1,8 @@
+import { EventData } from 'data/observable';
+
+declare let de: any;
+
+export function navigatingTo(args: EventData) {
+
+    console.dir(de.mannodermaus.rxbonjour.BonjourEvent);
+}
