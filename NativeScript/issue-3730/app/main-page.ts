ae01366da0514efb4fecd044bd46f83447ba3b03
NickIliev <nikolay.iliev@telerik.com>
2018-07-05 14:44:07 +0300

    add

--- NativeScript/issue-3730/app/main-page.ts
+++ NativeScript/issue-3730/app/main-page.ts
@@ -1,8 +1,12 @@
-import { EventData } from 'data/observable';
+import { EventData, fromObject } from 'data/observable';
+import { Page } from "ui/page";
 
 declare let de: any;
 
 export function navigatingTo(args: EventData) {
+    const page = <Page>args.object;
 
     console.dir(de.mannodermaus.rxbonjour.BonjourEvent);
+
+    page.bindingContext = fromObject({"msg": de.mannodermaus.rxbonjour.BonjourEvent["class"]});
 }
