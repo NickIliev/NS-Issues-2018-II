fad161c750b076eb15391aa5c1f98d3af6d971e0
NickIliev <nikolay.iliev@telerik.com>
2018-11-16 13:44:37 +0200

    add

--- others/modal-test/app/modal-view/modal-view-page.js
+++ others/modal-test/app/modal-view/modal-view-page.js
@@ -6,14 +6,14 @@ function onShownModally(args) {
     closeCallback = args.closeCallback;
     const view = args.object;
     view.bindingContext = observableModule.fromObject(context);
-
-    view.width = 300; // dynamically changing the default size of the root ccontainer of our modal view
-    view.height = 400; // dynamically changing the default size of the root ccontainer of our modal view 
+    
+    // view.width = 300; // dynamically changing the default size of the root ccontainer of our modal view
+    // view.height = 400; // dynamically changing the default size of the root ccontainer of our modal view 
 }
 exports.onShownModally = onShownModally;
 
 function onLoginButtonTap(args) {
-    const view = args.object;
+    const view = args.object.parent;
     const bindingContext = view.bindingContext;
     const username = bindingContext.get("username");
     const password = bindingContext.get("password");
