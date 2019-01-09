37b7b3a1d091f09029d43d313ebc83c93d8d0523
NickIliev <nikolay.iliev@telerik.com>
2019-01-09 11:39:36 +0200

    add

---
+++ others/t-1382291/app/main-page.ts
@@ -0,0 +1,22 @@
+import { EventData } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
+import { Label } from "tns-core-modules/ui/label";
+
+export function navigatingTo(args: EventData) {
+    let page = <Page>args.object;
+}
+
+export function onPageLoaded(args: EventData) {
+    let label = new Label();
+    console.log(`label: ${label}`);
+
+    label.text = "some text";
+
+    // let page = args.object as Page;
+    // page.content = label;
+
+    setTimeout(() => {
+        let nativeLabel = label.nativeView;
+        console.log(`nativeLabel: ${nativeLabel}`);
+    }, 300);
+}
